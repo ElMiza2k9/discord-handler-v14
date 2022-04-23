58754ecef9741c7bc754f350371062575dc959a7
DrGato <43690887+drgatoxd@users.noreply.github.com>
2022-04-23 11:24:58 -0500

    fix(handlers): command loop

--- src/lib/handlers/commands.ts
+++ src/lib/handlers/commands.ts
@@ -8,15 +8,19 @@ export function CommandHandler(client: LunarBot) {
 
 	const basedir = join(process.cwd(), 'src', 'commands');
 	const categories = readdirSync(basedir);
-	const commandPaths = categories.map(dir => readdirSync(join(basedir, dir)).filter(x => x.endsWith('.ts'))).flat();
 
-	commandPaths.forEach((path, i) => {
-		const command: Command = new (require(join(basedir, categories[i], path)).default)();
-		client.commands.set(command.options.name, command);
-		commands.push(command.options);
+	categories.forEach(category => {
+		const commandNames = readdirSync(join(basedir, category)).filter(x => x.endsWith('.ts'));
+
+		commandNames.forEach(commandName => {
+			const command: Command = new (require(join(basedir, category, commandName)).default)();
+			client.commands.set(command.options.name, command);
+			commands.push(command.options);
+		});
 	});
 
 	client.on('ready', () => {
 		client.guilds.cache.forEach(g => g.commands.set(commands).catch(() => null));
 	});
 }
+
